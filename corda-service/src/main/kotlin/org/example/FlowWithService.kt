de45cb76db72497c3d400e93c5052baa65d5cb0f
Zoltan Altfatter <altfatterz@gmail.com>
2019-11-24 19:00:43 +0100

    not sure how to start the service

--- corda-service/src/main/kotlin/org/example/FlowWithService.kt
+++ corda-service/src/main/kotlin/org/example/FlowWithService.kt
@@ -1,6 +1,7 @@
 package org.example
 
 import clojure.java.api.Clojure
+import clojure.lang.IFn
 import co.paralleluniverse.fibers.Suspendable
 import net.corda.core.flows.FlowLogic
 import net.corda.core.flows.StartableByRPC
@@ -26,12 +27,34 @@ class ExampleService(private val serviceHub: AppServiceHub) : SingletonSerialize
 
     private val log = LoggerFactory.getLogger(ExampleService::class.java)
 
+    private val requiringResolve = Clojure.`var`("clojure.core/requiring-resolve")
+
+    private fun resolve(symbolName: String): IFn? { // use new thread safe require / resolve combo
+        return requiringResolve.invoke(Clojure.read(symbolName)) as IFn
+    }
+
     init {
+        log.info("Creating instance of ExampleService...")
+
         val plus = Clojure.`var`("clojure.core", "+")
         val result = plus.invoke(1, 2)
-
         log.info("Result of Clojure calculation: $result")
-        log.info("Creating instance of ExampleService...")
+
+        val server = Clojure.`var`("server/start")
+        log.info("server: $server")
+
+        // Attempting to call unbound fn: #'server/start
+//        val invoke = resolve("server/start")?.invoke()
+//        log.info("invoke: $invoke")
+
+        // java.io.FileNotFoundException: Could not locate server__init.class, server.clj or server.cljc on classpath.
+//        val invoke = resolve("server/-main")?.invoke()
+//        log.info("invoke: $invoke")
+
+        // Not a qualified symbol: clojure_service.server
+//        val invoke = resolve("clojure_service.server")?.invoke()
+//        log.info("invoke: $invoke")
+
     }
 
     fun greet(): String {
